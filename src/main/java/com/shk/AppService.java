626a86a711835e6617724f8adc686c94e1bf8d54
DESKTOP-QH3TSPP\kk <572728626@qq.com>
2019-02-20 15:06:00 +0800

    二次提交

--- src/main/java/com/shk/AppService.java
+++ src/main/java/com/shk/AppService.java
@@ -2,11 +2,18 @@ package com.shk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppService {
+public class AppService extends SpringBootServletInitializer {
     public static void main(String[] args){
 
         SpringApplication.run(AppService.class,args);
     }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppService.class);
+    }
+
 }
